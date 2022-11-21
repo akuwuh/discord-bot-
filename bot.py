72caa3f4cf7b78e5810d724c07eae218273cd4ea
isaac <60953696+akuwuh@users.noreply.github.com>
2022-11-21 18:11:16 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,10 @@
+#token = MTA0NDEzODk1MDIyMDA3NTA0OQ.GadAM2.E9akhrufbeH4ry5EsfdWgeUtO4F44h0TL_chIw
+
+import hikari
+
+bot = hikari.GatewayBot(token="MTA0NDEzODk1MDIyMDA3NTA0OQ.GadAM2.E9akhrufbeH4ry5EsfdWgeUtO4F44h0TL_chIw", intents=hikari.Intents.ALL)
+
+#hi
+
+bot.run()
+
